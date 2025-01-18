71bbf636b4d17a1ec475e83076e413cb5a3bfaa0
logan-tolbert <lodev205@gmail.com>
2025-01-18 10:20:17 -0600

    :construction: Update '/json' route handler
    - Add condition to change the response output based on environment variable set in the .env file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require('express');
 const res = require('express/lib/response');
+require('dotenv').config();
 let app = express();
 const viewsDir = __dirname + '/views/index.html';
 const publicDir = __dirname + '/public';
@@ -7,10 +8,24 @@ const publicDir = __dirname + '/public';
 // app.get("/", (req, res)=>{
 //     res.send('Hello Express');
 // })
+
 app.use('/public', express.static(publicDir));
+
 app.get("/", (req,res)=>{
     res.sendFile(viewsDir);
 })
+
+app.get("/json", (reg,res)=>{
+    res.json(
+        process.env.MESSAGE_STYLE === 'uppercase' ? 
+        {"message": "HELLO JSON"} : {"message": "Hello json"}
+    );
+})
+
+
+
+
+
 
 
 
